a64ffdb1afa5e001a53634ea726ff778bc7770b6
JasonPanosso <jasonpanosso@protonmail.com>
2023-09-11 14:34:29 -0400

    more 2 pointers

---
+++ src/problems/two_pointers/max_number_of_k_sum_pairs.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn max_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut num_counts: HashMap<&i32, i32> = HashMap::new();
+        let mut operation_count = 0;
+
+        for num in nums.iter() {
+            match num_counts.get_mut(&(k - num)) {
+                Some(count) if *count > 0 => {
+                    *count -= 1;
+                    operation_count += 1
+                }
+                _ => *num_counts.entry(num).or_default() += 1,
+            }
+        }
+
+        operation_count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_1() {
+        let nums = vec![1, 2, 3, 4];
+        let k = 5;
+        let expected = 2;
+
+        assert_eq!(Solution::max_operations(nums, k), expected);
+    }
+
+    #[test]
+    fn test_example_2() {
+        let nums = vec![3, 1, 3, 4, 3];
+        let k = 6;
+        let expected = 1;
+
+        assert_eq!(Solution::max_operations(nums, k), expected);
+    }
+
+    #[test]
+    fn test_example_3() {
+        let nums = vec![2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2];
+        let k = 3;
+        let expected = 4;
+
+        assert_eq!(Solution::max_operations(nums, k), expected);
+    }
+}
--- src/problems/two_pointers/mod.rs
+++ src/problems/two_pointers/mod.rs
@@ -1,5 +1,6 @@
 pub mod container_with_most_water;
 pub mod is_subsequence;
+pub mod max_number_of_k_sum_pairs;
 pub mod move_zeroes;
 pub mod three_sum;
 pub mod trapping_rain_water;
